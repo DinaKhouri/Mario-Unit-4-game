e441315f55fe7d33223936cd1becc870cbee2d48
Dina Khouri <dina.khouri.91@gmail.com>
2019-01-03 15:19:01 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var sound_lose = document.getElementById("sound_lose");
 randomNumber = Math.floor(Math.random() * 101 + 19);
 console.log(randomNumber);
 $("#number").html(randomNumber);
+//computer generates values for each of the gems
 randomBlock1 = Math.floor(Math.random() * 15 + 1);
 console.log(randomBlock1);
 randomBlock2 = Math.floor(Math.random() * 15 + 1);
@@ -35,66 +36,14 @@ function reset() {
   randomBlock4 = Math.floor(Math.random() * 15 + 1);
   console.log(randomBlock4);
 }
-//computer generates values for each of the gems that can add up to equal the first random number
-//on click event for each of the buttons(click keys to move mario)
-// source https://www.tutorialrepublic.com/faq/move-an-element-to-left-right-up-and-down-using-arrow-keys-in-jquery.php
-// $(document).keydown(function(e) {
-//   switch (e.which) {
-//     case 37: //left arrow key
-//       $(".mario")
-//         .finish()
-//         .animate({
-//           left: "-=50"
-//         });
-//       break;
-//     case 38:
-//up arrow key //to make it jump i had to add 2 animate lines:
-//one to get it to go up, and followed by another one in the negative value to make it go back down in the same value
 
-//       $(".mario")
-//         .finish()
-//         .animate({
-//           top: "-=115"
-//         });
-//       $(".mario")
-//         .finish()
-//         .animate({
-//           top: "+=115"
-//         });
-//       sound_jump.play();
-//       var posMario = $(".mario").position();
-//       console.log(posMario);
-
-//       break;
-//     case 39: //right arrow key
-//       $(".mario")
-//         .finish()
-//         .animate({
-//           left: "+=50"
-//         });
-//       break;
-//   }
-// });
 //got mario to move with mouseX from the source  https://stackoverflow.com/questions/40394424/jquery-how-to-make-a-div-follow-a-cursor-within-its-center/40394477
 $(document).bind("mousemove", function(e) {
   $(".mario").css({
-    left: e.pageX - $(".container").width() / 6 // just minus by half the width
+    left: e.pageX - $(".container").width() / 6 //kept on changing the number till it works with my screen dimensions and its right under the block
   });
 });
 
-//now i need to add a collision affect that trigers the value of the block that mario hits
-
-var pos1 = $("#block1").position();
-console.log(pos1);
-
-var pos2 = $("#block2").position();
-console.log(pos2);
-
-var pos3 = $("#block3").position();
-console.log(pos3);
-
-var pos4 = $("#block4").position();
-console.log(pos4);
 //create a function for mario jump to call it inside the mouse on click function
 function marioJump() {
   $(".mario")
@@ -113,6 +62,15 @@ function marioJump() {
 //on click event adds the number to the score
 $("#block1").on("click", function() {
   marioJump();
+  //shake the block source https://www.youtube.com/watch?v=CBQGl6zokMs
+  $("#block1")
+    .addClass("animated bounce")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass("animated bounce");
+      }
+    );
 
   console.log("clicked");
   score += randomBlock1;
@@ -134,6 +92,14 @@ $("#block1").on("click", function() {
 
 $("#block2").on("click", function() {
   marioJump();
+  $("#block2")
+    .addClass("animated bounce")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass("animated bounce");
+      }
+    );
   console.log("clicked");
   score += randomBlock2;
   $(".score").html(score);
@@ -154,6 +120,14 @@ $("#block2").on("click", function() {
 
 $("#block3").on("click", function() {
   marioJump();
+  $("#block3")
+    .addClass("animated bounce")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass("animated bounce");
+      }
+    );
   console.log("clicked");
   score += randomBlock3;
   $(".score").html(score);
@@ -174,6 +148,14 @@ $("#block3").on("click", function() {
 
 $("#block4").on("click", function() {
   marioJump();
+  $("#block4")
+    .addClass("animated bounce")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass("animated bounce");
+      }
+    );
   console.log("clicked");
   score += randomBlock4;
   $(".score").html(score);
